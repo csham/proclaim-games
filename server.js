1240380228b636f24fedff6a498b7596862902d3
Chris Sham <cbs1918@gmail.com>
2022-08-11 13:51:16 -0400

    Adding sound fx and highlighting winnder.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./build/'));
+app.use(express.static('./docs/'));
 
 var port = process.argv[2] || 8088;
 app.listen(port);
--- src/js/app.js
+++ src/js/app.js
@@ -4,29 +4,69 @@
         canSpin: true,
         wheelSlices: 6,
         rotationTime: 3000,
-        wheelSprite: null,
-        revealerSprite: null
+        sprites: {
+            wheel: null,
+            revealer: null
+        },
+        audio: {
+            beep: null
+        },
+        inputKeys: null,
+    };
+
+    var blinkRevealer = function() {
+        var animatedTweens = [];
+        for (var i = 0; i < 10; i++) {
+            animatedTweens.push({
+                targets: [GameData.sprites.revealer],
+                duration: 1,
+                alpha: (i % 2),
+                offset: (i * 150)
+            });
+        }
+        GameData.game.tweens.timeline({
+            tweens: animatedTweens,
+            onComplete: function() {
+                GameData.canSpin = true;
+            }
+        });
     };
     
     var spinWheel = function() {
         if (GameData.canSpin) {
-            GameData.revealerSprite.alpha = 0;
+            GameData.canSpin = false;
+            GameData.sprites.revealer.alpha = 0;
             var rounds = Phaser.Math.Between(2, 4);
             var degrees = Phaser.Math.Between(0, 360);
             var angle = (360 * rounds) + degrees;
-            GameData.canSpin = false;
+            var revealerAngle = degrees;
+            while (revealerAngle > 30) {
+                revealerAngle -= 60;
+            }
 
+            var previousAngle = 0;
             GameData.game.tweens.add({
-                targets: [GameData.wheelSprite],
+                targets: [GameData.sprites.wheel],
                 angle: angle,
                 duration: GameData.rotationTime,
                 ease: "Cubic.easeOut",
                 onComplete: function(tween) {
-                    GameData.canSpin = true;
+                    GameData.sprites.revealer.angle = revealerAngle;
+                    blinkRevealer();
+                },
+                onUpdate: function(tween, target) {
+                    var currentAngle = Math.floor(target.angle) + 389; // 389 = 360 + 30 (starting wedge offset) - 1 so we don't play sound when landing on the line but don't count the next number as the answer
+                    while (currentAngle > 360) {
+                        currentAngle -= 360;
+                    }
+                    currentAngle = Math.floor(currentAngle / 60) * 60;
+                    
+                    if (currentAngle != previousAngle) {
+                        GameData.audio.beep.play();
+                        previousAngle = currentAngle;
+                    }
                 }
             });
-            GameData.revealerSprite.angle = (degrees % 60);
-            // GameData.revealerSprite.alpha = 1;
         }
     };
 
@@ -37,6 +77,11 @@
         GameData.game.load.image('spinner', 'assets/img/spinner.png');
         GameData.game.load.image('revealer', 'assets/img/revealer.png');
         GameData.game.load.image('arrow', 'assets/img/arrow.png');
+
+        GameData.game.load.audio({
+            key: 'beep',
+            url: ['assets/audio/mixkit-tech-break-fail-2947.wav']
+        });
     };
 
     var create = function() {
@@ -44,12 +89,15 @@
         var screenCenterY = GameData.game.cameras.main.worldView.y + (GameData.game.cameras.main.height/2);
         GameData.game.add.image(screenCenterX, screenCenterY, 'background');
 
-        GameData.wheelSprite = GameData.game.add.image(screenCenterX, screenCenterY, 'spinner');
-        GameData.revealerSprite = GameData.game.add.image(screenCenterX, screenCenterY, 'revealer');
-        GameData.revealerSprite.alpha = 0;
+        GameData.sprites.wheel = GameData.game.add.image(screenCenterX, screenCenterY, 'spinner');
+        GameData.sprites.revealer = GameData.game.add.image(screenCenterX, screenCenterY, 'revealer');
+        GameData.sprites.revealer.alpha = 0;
         GameData.game.add.image(screenCenterX, screenCenterY, 'arrow');
 
+        GameData.audio.beep = GameData.game.sound.add('beep');
+
         GameData.game.input.on("pointerdown", spinWheel);
+        GameData.game.input.keyboard.on("keydown-SPACE",  spinWheel);
     };
 
     var config = {
